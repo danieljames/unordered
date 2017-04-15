65932665a65eefda91783576884317003f50b432
Daniel James <daniel@calamity.org.uk>
2017-04-15 17:35:10 +0100

    Remove early exit for empty containers

--- include/boost/unordered/detail/implementation.hpp
+++ include/boost/unordered/detail/implementation.hpp
@@ -3082,14 +3082,9 @@ struct table : boost::unordered::detail::functions<typename Types::hasher,
         mlf_ = x.mlf_;
         recalculate_max_load();
 
-        if (!size_ && !x.size_) {
-            new_func_this.commit();
-            return;
-        }
-
         if (x.size_ >= max_load_) {
             create_buckets(min_buckets_for_size(x.size_));
-        } else {
+        } else if (size_) {
             clear_buckets();
         }
 
@@ -3169,14 +3164,9 @@ struct table : boost::unordered::detail::functions<typename Types::hasher,
             mlf_ = x.mlf_;
             recalculate_max_load();
 
-            if (!size_ && !x.size_) {
-                new_func_this.commit();
-                return;
-            }
-
             if (x.size_ >= max_load_) {
                 create_buckets(min_buckets_for_size(x.size_));
-            } else {
+            } else if (size_) {
                 clear_buckets();
             }
 
